7cfe7c746d24656be38cb10d504c1c15094553fe
syosyo10337 <syosyo.ds@gmail.com>
2022-09-29 01:01:46 +0900

    Devise導入とリンクを貼る#6

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
+  devise_for :users, controllers: {
+    # confirmations: 'users/confirmations',
+    # omniauth_callbacks: 'users/omniauth_callbacks',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    # unlocks: 'users/unlocks'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
