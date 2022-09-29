68b603d1f174400748bee8eb77c4766104fe0a56
syosyo10337 <syosyo.ds@gmail.com>
2022-09-29 19:10:33 +0900

    フォーム画面のスタイル#6

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,10 +5,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  def new
-    super
-    @hello ="hellp"
-  end
+  # def new
+  #   super
+  # end
 
   # POST /resource
   # def create
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,6 +5,7 @@ class Users::SessionsController < Devise::SessionsController
 
   #GET /resource/sign_in
   # def new
+
   #   super
   # end
 
