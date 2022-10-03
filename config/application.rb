412a14393284dcd4215f5a6a17b53d92181cd9cd
syosyo10337 <syosyo.ds@gmail.com>
2022-10-03 19:35:05 +0900

    error/flashメッセージのスタイリング

--- config/application.rb
+++ config/application.rb
@@ -11,13 +11,13 @@ module RecordaMe
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.i18n.default_locale = :ja
+    config.time_zone = "Tokyo"
 
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
