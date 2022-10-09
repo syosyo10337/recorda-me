59c799a1243f3e632f780294992aa6848ef1bf7a
syosyo10337 <syosyo.ds@gmail.com>
2022-10-09 14:46:26 +0900

    日本語化対応をブラウザで確認

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
-  devise_for :users, controllers: {
-    passwords: 'users/passwords',
-    registrations: 'users/registrations',
-    sessions: 'users/sessions'
-  }
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
