b493b43f4f70f09f3f5e8d511db261c2c7d83002
syosyo10337 <syosyo.ds@gmail.com>
2022-11-03 20:16:58 +0900

    項目のラベルの色を出し分け。
    item_numヘルパーとcssに色をセット

--- app/helpers/item_helper.rb
+++ app/helpers/item_helper.rb
@@ -1,2 +1,7 @@
 module ItemHelper
+
+  # itemを引数に取り,userがもつ何個目のitemであるかを返す。(1-origin)
+  def item_num(item)
+    @items.pluck(:id).find_index(item.id) + 1
+  end
 end
--- spec/system/edit_item_spec.rb
+++ spec/system/edit_item_spec.rb
@@ -11,8 +11,7 @@ RSpec.describe 'EditItems', type: :system do
     click_on '活動項目'
     expect(page).to have_content '記録中の活動項目一覧'
     # 1つの目の項目を編集する
-    item_id = user.items.first.id
-    find("#edit-item-#{item_id}").click
+    find("#edit-item-1").click
 
     fill_in 'item_name', with: '新項目'
     click_on '更新する'
