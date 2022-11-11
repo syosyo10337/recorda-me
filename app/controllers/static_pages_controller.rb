d0d4d844f9df5fae4fe9bfc6062dcaf1dde0fb41
syosyo10337 <syosyo.ds@gmail.com>
2022-11-12 01:56:57 +0900

    add terms_of_service and privacy_policy

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,15 @@
 class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!
-  
+
   def home
   end
 
   def about
+  end
+
+  def privacy
+  end
+
+  def service
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,6 @@ class UsersController < ApplicationController
   before_action :set_feed_logs, only: %i[home]
   before_action :set_items_includes_logs, only: %i[home]
 
-  # 仮で実装しました。
-  def index
-    @users = User.all
-  end
-
   def home
     @log = current_user.logs.build
   end
