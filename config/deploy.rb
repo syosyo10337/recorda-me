f5789f0de872296a7e81b82161de7773fbc09e6e
syosyo10337 <syosyo.ds@gmail.com>
2022-12-04 10:07:22 +0000

    setup capistrano for CD from my local

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,14 +10,13 @@ set :rbenv_ruby, File.read('.ruby-version').strip
 set :keep_releases, 5
 
 append :linked_files, 'config/master.key'
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'node_modules'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'node_modules', 'public'
 
 # Nginxの設定ファイル名と置き場所を修正
-set :nginx_config_name, 'rails.conf'
-set :nginx_sites_enabled_path, '/etc/nginx/conf.d'
+set :nginx_config_name, "#{fetch(:application)}.conf"
+set :nginx_sites_enabled_path, '/etc/nginx/conf.d/'
 # Default value for default_env is {}
 # set :default_env, { path: '/opt/ruby/bin:$PATH' }
 set :pty, true
 set :puma_enable_socket_service, true
-
 # set :ssh_options, verify_host_key: :secure
