41cd4ddc060320eca55f844efcfbaebd076796a8
syosyo10337 <syosyo.ds@gmail.com>
2023-01-13 14:57:33 +0900

    modify modal style

--- app/javascript/components/Modal.jsx
+++ app/javascript/components/Modal.jsx
@@ -8,10 +8,10 @@ const Modal = ({closeModal, title, currentName}) => {
           <h1 class="text-center">{title}</h1>
         </div>
         <div class="modal-message my-3">
-          <input class="form-control d-inline" type="text" value={currentName} name="item[name]" id="item_name" />
+          <input class="form-control d-inline" type="text" value={currentName} name="item[name]" id="item_name_input" />
         </div>
         <div class="text-end">
-          <input type="submit" name="commit" value="更新する" class="btn btn-secondary" />
+          <input type="submit" name="commit" value="更新する" class="btn btn-secondary me-1" />
           <a className="btn btn-outline-secondary" onClick={closeModal}>閉じる</a>
         </div>
       </div>
