4608afee4f8d76c02ccf502d7d7dcca3a9cb7958
syosyo10337 <syosyo.ds@gmail.com>
2023-01-21 01:31:33 +0900

    不要になったrails_viewを削除#78

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,41 +1,19 @@
 class ItemsController < ApplicationController
-  # @itemに代入
-  before_action :set_item, only: %i[show edit update]
-  before_action :item_owner?, only: %i[show edit update]
   before_action :set_feed_logs, only: %i[show]
   # @itemsに代入
   before_action :set_items_includes_logs, only: %i[show]
 
-  def index
-    @items = current_user.items.get_fixed_order
-  end
+  def index; end
 
-  # 項目にまつわる記録を表示 /item/:id
   def show
+    set_item
+    item_owner?
     @feed_logs = @feed_logs.where(item_id: @item.id)
     # log_formのため
     @log = current_user.logs.build
   end
 
-  # 項目名編集画面
-  def edit; end
-
-  def update
-    if @item.update(items_params)
-      redirect_to items_path, notice: '名前を更新しました。'
-    else
-      # 更新失敗時に、DB上の名前を取得する
-      @item.reload
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
   private
-
-  # strongparameters/name属性のみ許可
-  def items_params
-    params.require(:item).permit(:name)
-  end
 
   def set_item
     @item = Item.find(params[:id])
