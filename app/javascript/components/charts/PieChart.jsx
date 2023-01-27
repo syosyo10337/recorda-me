b577a841b5636e2113040116628a34f79af086c5
syosyo10337 <syosyo.ds@gmail.com>
2023-01-28 01:07:39 +0900

    2つのチャートを描画し、切り替え流ようにした#74

--- app/javascript/components/charts/PieChart.jsx
+++ app/javascript/components/charts/PieChart.jsx
@@ -1,33 +1,34 @@
-import React,{ useEffect, useState } from "react";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Pie } from 'react-chartjs-2';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+import axios from "axios";
 
+// TODO: lineチャートでも使うため、一箇所にまとめてimportして使うようにする
 const ITEM_COLORS = [
-  '#D896AD',
-  '#ECA2A3',
-  '#F1B996',
-  '#F6D699',
-  '#E3DE9A',
-  '#A6D29A',
-  '#93C7B7',
-  '#92BFC7',
-  '#90B0CA',
-  '#B5A4C7',
-  '#B5A4C7',
-  '#C59DBB',
-]
+  "#D896AD",
+  "#ECA2A3",
+  "#F1B996",
+  "#F6D699",
+  "#E3DE9A",
+  "#A6D29A",
+  "#93C7B7",
+  "#92BFC7",
+  "#90B0CA",
+  "#B5A4C7",
+  "#B5A4C7",
+  "#C59DBB",
+];
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 function PieChart() {
-  const[data, setData] = useState([]);
-  const fetch = async () => {
-    const resp = await axios.get('/api/charts/all_pies')
-    setData(resp.data)
-  }
+  const [data, setData] = useState([]);
+  const fetchData = async () => {
+    const resp = await axios.get("/api/charts/all_pies");
+    setData(resp.data);
+  };
   useEffect(() => {
-    fetch()
-  }, [])
+    fetchData();
+  }, []);
 
   const chartData = {
     labels: data.map(ele => ele[0]),
@@ -36,23 +37,21 @@ function PieChart() {
         label: data.map(ele => ele[0]),
         data: data.map(ele => ele[1]),
         backgroundColor: ITEM_COLORS,
-      }
+      },
     ],
   };
   const options = {
     plugins: {
       tooltip: {
         callbacks: {
-          label: context => `${context.label}: ${context.parsed}時間`
-        }
-      }
+          label: context => `${context.label}: ${context.parsed}時間`,
+        },
+      },
     },
-    maintainAspectRatio: false
-  }
+    maintainAspectRatio: false,
+  };
 
-  return (
-    <Pie data={chartData} options={options} height={300}/>
-  )
+  return <Pie data={chartData} options={options} height={400} />;
 }
 
 export default PieChart;
--- app/javascript/components/pages/Stats.jsx
+++ app/javascript/components/pages/Stats.jsx
@@ -1,21 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import PieChart from "../charts/PieChart";
+import LineChart from "../charts/LineChart";
 
 const Stats = () => {
+  const [toggle, switchToggle] = useState({
+    "switch" : 1,
+    "pieStatus": "active",
+    "lineStatus": ""
+  });
+  const activatePie = () => {
+    switchToggle({
+      "switch" : 1,
+      "pieStatus": "active",
+      "lineStatus": ""
+    })
+  };
+  const activateLine = () => {
+    switchToggle({
+      "switch" : 0,
+      "pieStatus": "",
+      "lineStatus": "active"
+    })
+  };
   return (
     <div className="card text-center">
       <div className="card-header">
         <ul className="nav nav-tabs card-header-tabs">
           <li className="nav-item">
-            <a href="#" className="nav-link active">比率を見る</a>
+            <button
+              onClick={activatePie}
+              className={`nav-link ${toggle.pieStatus}`}>
+              比率を見る
+              </button>
           </li>
           <li className="nav-item">
-            <a href="#" className="nav-link">遷移を見る</a>
+            <button
+              onClick={activateLine}
+              className={`nav-link ${toggle.lineStatus}`}>
+              遷移を見る
+              </button>
           </li>
         </ul>
       </div>
       <div className="card-body">
-        <PieChart />
+        {toggle.switch ? <PieChart /> : <LineChart />}
       </div>
     </div>
   )
