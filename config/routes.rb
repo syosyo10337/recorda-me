a11f6db59bac2d9bb30ccfff5093db19a1004c6b
syosyo10337 <syosyo.ds@gmail.com>
2023-01-29 21:15:37 +0900

    routingを修正#74

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get '/about', to: 'static_pages#about'
-  get '/privacy_policy', to: 'static_pages#privacy'
-  get '/terms_of_service', to: 'static_pages#service'
-  root 'static_pages#home'
-  post '/guest', to: 'users#guest'
   get '/stats', to: 'statistics#index'
 
   resources :items, only: %i[index show]
@@ -16,11 +11,17 @@ Rails.application.routes.draw do
   authenticated :user do
     root 'users#home', as: :authenticated_root
   end
+
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
   get '/users', to: 'users#registrations'
-  # api関連のルーティング
+  get '/about', to: 'static_pages#about'
+  get '/privacy_policy', to: 'static_pages#privacy'
+  get '/terms_of_service', to: 'static_pages#service'
+  root 'static_pages#home'
+  post '/guest', to: 'users#guest'
+
   namespace :api do
     resources :items, only: %i[index update]
     namespace :stats do
